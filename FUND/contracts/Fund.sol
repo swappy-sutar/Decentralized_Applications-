e1d3bad8642521d4f21754aea440ba2fef439bb2
swapnil <sutarswapnil322@gmail.com>
2024-06-22 11:24:06 +0530

    create project fund and create react and truffle init

---
+++ FUND/contracts/Fund.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract fund {
+
+    uint public numberOfFunders;
+
+    mapping(uint=>address)private funders;
+
+    receive() external payable{};
+
+    function transfer()external payable {
+        funders[numberOfFunders] = msg.sender;
+    }
+
+    function withdraw(uint withdrawAmount) external{
+        require(withdrawAmount < 2000000000000000000,"Cannot withdraw more than 2Ether");
+        payable(msg.sender).trasfer(withdrawAmount);
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,10 @@
+#react with Vite:
+npm create vite@latest
+
+#install truffle
+
+    npm install -g truffle
+    truffle version
+
+#truffle initialise in root folder
+truffle init
